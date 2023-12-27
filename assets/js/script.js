1228183a4cf271af713a713d719f1f980efb21ef
Rupthuz <>
2023-12-27 17:22:11 +0000

    add event listener for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,40 @@
-function runGame () {
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("you clicked Submit!");
+            } else {
+                let gametype = this.getAttribute("data-type");
+                alert(`you clicked ${gametype}`);
+            }
+        });
+    }
+});
+
+
+function runGame() {
 
 }
 
-function checkAnswer () {
+function checkAnswer() {
 
 }
 
-function calculateCorrectAnwer () {
+function calculateCorrectAnwer() {
 
 }
 
-function incrementScore () {
+function incrementScore() {
 
 }
 
-function incrementWrongAnswer () {
+function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion () {
+function displayAdditionQuestion() {
 
 }
 
