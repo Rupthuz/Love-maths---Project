9af69dfa2c2589c5086eb6aa42e49df7d3b5b29a
Rupthuz <>
2023-12-28 15:33:12 +0000

    add functions to addition questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,27 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("you clicked Submit!");
             } else {
                 let gametype = this.getAttribute("data-type");
-                alert(`you clicked ${gametype}`);
+                runGame(gametype);
             }
         });
     }
+
+    runGame("addition");
+
 });
 
 
-function runGame() {
+function runGame(gametype) {
 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gametype === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gametype}`);
+        throw `Unknown game type: ${gametype}. Aborting!`;
+    }
 
 }
 
@@ -37,8 +47,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
